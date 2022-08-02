409362dc78ee3bb44cd2bdfd95fd903e56e05963
Jiacheng Li <cheng2029@foxmail.com>
2022-08-03 01:11:02 +0800

    Mastering Connection-Level APIs - August 3rd, 2022

--- src/main/scala/part4client/ConnectionLevelAPI.scala
+++ src/main/scala/part4client/ConnectionLevelAPI.scala
@@ -1,21 +1,21 @@
 package part4client
 
 import akka.NotUsed
-import akka.actor.typed.{ActorSystem, Scheduler}
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Scheduler}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-import akka.stream.scaladsl.Sink
-import akka.stream.scaladsl.{Flow, Source}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model._
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.{Materializer, SystemMaterializer}
 import com.typesafe.config.ConfigFactory
+import part4client.PaymentSystemDomain._
+import spray.json._
 
-import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
-import scala.util.{Failure, Success}
 
-object ConnectionLevelAPI {
+object ConnectionLevelAPI extends PaymentJsonProtocol with SprayJsonSupport {
 
   implicit val system: ActorSystem[NotUsed] = ActorSystem(
     Behaviors.empty[NotUsed], "ConnectionLevelAPI",
@@ -33,17 +33,40 @@ object ConnectionLevelAPI {
   def oneOffRequest(request: HttpRequest): Future[HttpResponse] =
     Source.single(request).via(connectionFlow).runWith(Sink.head)
 
+  //  oneOffRequest(HttpRequest()).onComplete {
+  //    case Success(response) => println(s"Got successful response: $response")
+  //    case Failure(exception) => println(s"Sending the request failed: $exception")
+  //  }
+  //  scheduler.scheduleOnce(1 second, () => system.terminate())
+
   /**
    * Real-Production Scenario:
    * - A Small payments system
    */
+  val creditCards: List[CreditCard] = List(
+    CreditCard("4242-4242-4242-4242", "424", "tx-test-account"),
+    CreditCard("1234-1234-1234-1234", "123", "tx-ken-account"),
+    CreditCard("1234-1234-4321-4321", "321", "my-awesome-account"),
+  )
 
   def main(args: Array[String]): Unit = {
-    oneOffRequest(HttpRequest()).onComplete {
-      case Success(response) => println(s"Got successful response: $response")
-      case Failure(exception) => println(s"Sending the request failed: $exception")
-    }
+    val httpRequests: List[HttpRequest] =
+      creditCards
+        .map((card: CreditCard) => PaymentRequest(card, "elapsed-store-account", 99))
+        .map((request: PaymentRequest) =>
+          HttpRequest(
+            HttpMethods.POST,
+            uri = Uri("/api/payments"),
+            entity = HttpEntity(
+              ContentTypes.`application/json`,
+              request.toJson.prettyPrint
+            )
+          )
+        )
 
-    scheduler.scheduleOnce(1 second, () => system.terminate())
+    Source(httpRequests)
+      .via(Http().outgoingConnection("localhost", 8080))
+      .to(Sink.foreach[HttpResponse](println))
+      .run()
   }
 }
--- src/main/scala/part4client/PaymentSystem.scala
+++ src/main/scala/part4client/PaymentSystem.scala
@@ -1,18 +1,35 @@
 package part4client
 
+import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.{StatusCode, StatusCodes}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.{Materializer, SystemMaterializer}
+import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
 import org.slf4j.Logger
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
+import scala.util.{Failure, Success}
 
 object PaymentSystemDomain {
   trait TransactionProtocol
-  final case class PaymentRequest(creditCard: CreditCard,
+  final case class TransactionRequest(creditCard: CreditCard,
                                   receiverAccount: String,
-                                  amount: Double, replyTo: ActorRef[TransactionProtocol]) extends TransactionProtocol
-  case object PaymentAccepted extends TransactionProtocol
-  case object PaymentRejected extends TransactionProtocol
+                                  amount: Double, replyTo: ActorRef[TransactionResponse]) extends TransactionProtocol
+  trait TransactionResponse
+  case object TransactionAccepted extends TransactionResponse
+  case object TransactionRejected extends TransactionResponse
 
-  final case class CreditCard(serialNumber: String, securityCode: String, senderAccount: String)
+  final case class CreditCard(serialNumber: String, securityCode: String, account: String)
+  final case class PaymentRequest(creditCard: CreditCard, receiverAccount: String, amount: Double)
 }
 
 object PaymentValidator {
@@ -24,12 +41,12 @@ object PaymentValidator {
       val logger: Logger = context.log
 
       message match {
-        case PaymentRequest(creditCard, receiverAccount, amount, replyTo) =>
+        case TransactionRequest(creditCard, receiverAccount, amount, replyTo) =>
           logger.info(s"[${context.self.path.name}] Handling ${message.getClass} message.")
-          logger.info(s"[${context.self.path.name}] ${creditCard.senderAccount} is trying " +
+          logger.info(s"[${context.self.path.name}] ${creditCard.account} is trying " +
             s"to send $amount to $receiverAccount")
-          if (creditCard.serialNumber == "1234-1234-1234-1234") replyTo ! PaymentRejected
-          else replyTo ! PaymentAccepted
+          if (creditCard.serialNumber == "1234-1234-1234-1234") replyTo ! TransactionRejected
+          else replyTo ! TransactionAccepted
 
           Behaviors.same
         case _ =>
@@ -40,9 +57,68 @@ object PaymentValidator {
   }
 }
 
-object PaymentSystem {
+trait PaymentJsonProtocol extends DefaultJsonProtocol {
+
+  import PaymentSystemDomain._
+
+  implicit val creditCardFormat: RootJsonFormat[CreditCard] = jsonFormat3(CreditCard)
+  implicit val paymentRequestFormat: RootJsonFormat[PaymentRequest] = jsonFormat3(PaymentRequest)
+}
+
+object PaymentSystem extends PaymentJsonProtocol with SprayJsonSupport {
+
+  import PaymentSystemDomain._
+
+  // akka microservice support
+  trait RootCommand
+  final case class RetrieveSystemActor(replyTo: ActorRef[ActorRef[TransactionProtocol]]) extends RootCommand
+
+  val rootBehavior: Behavior[RootCommand] = Behaviors.setup { context: ActorContext[RootCommand] =>
+    val systemActor: ActorRef[TransactionProtocol] = context.spawn(PaymentValidator(), "validator")
+
+    Behaviors.receiveMessage {
+      case RetrieveSystemActor(replyTo) =>
+        replyTo ! systemActor
+        Behaviors.same
+    }
+  }
+
+  implicit val system: ActorSystem[RootCommand] = ActorSystem(
+    rootBehavior, "PaymentSystem",
+    ConfigFactory.load().getConfig("my-config")
+  )
+  implicit val mat: Materializer = SystemMaterializer(system).materializer
+  implicit val timeout: Timeout = Timeout(5 seconds)
+  implicit val scheduler: Scheduler = system.scheduler
+  implicit val ec: ExecutionContext = system.executionContext
 
   def main(args: Array[String]): Unit = {
+    val retrieveValidatorFuture: Future[ActorRef[TransactionProtocol]] =
+      system.ask((replyTo: ActorRef[ActorRef[TransactionProtocol]]) => RetrieveSystemActor(replyTo))
 
+    retrieveValidatorFuture.onComplete {
+      case Success(validator) =>
+        val paymentRoute: Route = pathPrefix("api" / "payments") {
+          post {
+            entity(as[PaymentRequest]) { paymentRequest: PaymentRequest =>
+              val validationFuture: Future[StatusCode with Serializable] = validator.ask(
+                (replyTo: ActorRef[TransactionResponse]) => TransactionRequest(
+                  paymentRequest.creditCard,
+                  paymentRequest.receiverAccount,
+                  paymentRequest.amount,
+                  replyTo)
+              ).map {
+                case TransactionAccepted => StatusCodes.OK
+                case TransactionRejected => StatusCodes.Forbidden
+                case _ => StatusCodes.BadRequest
+              }
+              complete(validationFuture)
+            }
+          }
+        }
+        Http().newServerAt("localhost", 8080).bind(paymentRoute)
+      case Failure(exception) =>
+        system.log.error(s"Errors! Failed to retrieve validator: $exception")
+    }
   }
 }
